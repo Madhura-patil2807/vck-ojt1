80ad012a9c5f3e9bd11ab7c53f1989e58eeac6ee
Madhura-patil2807 <patilmadhur05@gmail.com>
2025-06-13 11:19:14 +0530

    tenth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import About from './pages/AboutPage'
 import Courses from './pages/CoursesPage'
@@ -9,9 +9,29 @@ import Admission from './pages/AdmissionPage'
 import './App.css'
 import ChatbotComponent from './components/Chatbot/ChatbotComponents'
 import Footer from './components/Footer/Footer'
+import DeveloperInfoPopup from './components/DevloperInfo/DeveloperInfoPopup'
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
+    <>
+    <div>
+      <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Madhura Ananda Patil"
+          studentPhotoUrl="/madhura1.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+    </div>
+    
     <div>
       <Router>
         <Routes>
@@ -35,6 +55,7 @@ const App = () => {
 
 
     </div>
+    </>
     
   )
 }
